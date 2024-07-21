752933e2d7285e74da383aaedc701f3ead77b99d
LarodarPenido <fabiotorlai@gmail.com>
2024-07-21 13:08:44 -0300

    breakable tiles are breakable

--- Scripts/spell.gd
+++ Scripts/spell.gd
@@ -11,7 +11,7 @@ extends CharacterBody2D
 var direction: Vector2
 var current_speed: Vector2
 
-
+const DESTRUCTIBLE = 1
 
 ##VFX
 ## instantiate sparks on death
@@ -20,20 +20,37 @@ func _ready():
 	life_timer.start(spell_lifetime)
 	current_speed = direction * spell_speed
 
-func _process(delta):
+func _physics_process(delta):
 	#var target_speed = spell_direction * spell_speed * delta
 	#current_speed = current_speed.lerp(target_speed, spell_acceleration * delta)  
 	velocity = current_speed * delta
 	move_and_collide(velocity)
+	
+	var collision_info = move_and_collide(velocity)
+	
+	if collision_info:
+		
+		var collider = collision_info.get_collider()
+		if collider is TileMap:
+			var collision_position = collision_info.get_position()
+			var map_position = collider.local_to_map(collision_position)
+			var tile_type_id = collider.get_cell_source_id(0, map_position)
+			if tile_type_id == DESTRUCTIBLE:
+				var atlas_position = collider.get_cell_atlas_coords(0, map_position)
+				if atlas_position.y == 0:
+					if atlas_position.x == 0:
+						collider.erase_cell(0, map_position)
+					else:
+						atlas_position.x -=1
+						collider.set_cell(0, map_position, tile_type_id, atlas_position)
+					
+
+		
+		
+		
+		queue_free()
+
 
 
 func _on_life_timer_timeout():
-	queue_free()
-
-
-func _on_collision_shape_2d_body_entered(body):
-	queue_free()
-
-
-func _on_collision_shape_2d_area_entered(area):
 	queue_free()
