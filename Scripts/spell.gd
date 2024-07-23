c775d10f7851cdd8df1065fb6bca7b57c4d660f3
LarodarPenido <fabiotorlai@gmail.com>
2024-07-22 21:25:35 -0300

    corrected tile destruction

--- Scripts/spell.gd
+++ Scripts/spell.gd
@@ -12,13 +12,12 @@ var current_speed: Vector2
 
 const DESTRUCTIBLE = 1
 
-
-
 const COLLECTABLE = preload("res://Scenes/Collectable.tscn")
 
 ##VFX
 ## instantiate sparks on death
 
+const BREAK_PARTICLES = preload("res://Scenes/break_particles.tscn")
 func _ready():
 	life_timer.start(spell_lifetime)
 	current_speed = direction * spell_speed
@@ -38,7 +37,8 @@ func _physics_process(delta):
 		if collider is TileMap:
 			var collision_position = collision_info.get_position()
 			print("Collision position:", collision_position)
-			var map_position = collider.local_to_map(collision_position)
+			#var map_position = collider.local_to_map(collision_position)
+			var map_position = collider.local_to_map(collision_position - collision_info.get_normal())
 			print("Map position:", map_position)
 			var tile_type_id = collider.get_cell_source_id(0, map_position)
 			print("Tile type ID:", tile_type_id)
@@ -48,8 +48,10 @@ func _physics_process(delta):
 				if atlas_position.y == 0:
 					if atlas_position.x == 0:
 						collider.erase_cell(0, map_position)
-						
+						spawn_collectable(collision_position)
+						spawn_sparks(collision_position)
 						# TODO
+						# add chance system and collectible counter
 						# play rock break sound
 						# spawn particles
 						# if chance, and collectable stack > 0, 
@@ -58,13 +60,19 @@ func _physics_process(delta):
 					else:
 						atlas_position.x -=1
 						collider.set_cell(0, map_position, tile_type_id, atlas_position)
-				spawn_collectable(collision_position)
+				
 				queue_free()
 
 func spawn_collectable(_position):
 	print("Collectable spawned at:", _position)
 	var collectable = COLLECTABLE.instantiate()
+	get_tree().root.add_child(collectable)
 	collectable.global_position = _position
+	
+func spawn_sparks(_position):
+	var sparks = BREAK_PARTICLES.instantiate()
+	get_tree().root.add_child(sparks)
+	sparks.global_position = _position
 
 func _on_life_timer_timeout():
 	queue_free()
